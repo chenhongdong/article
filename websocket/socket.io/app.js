33a267021c882f7a6113ab5f0b2108e4a9f96452
Lyujialin <1148043050@qq.com>
2018-09-04 23:38:24 +0800

    io广播

--- websocket/socket.io/app.js
+++ websocket/socket.io/app.js
@@ -6,15 +6,35 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/news', (req, res) => {
+    res.sendFile(path.join(__dirname, 'news.html'));
+});
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
-    console.log('客户端已经链接，我这里是服务端');
     socket.send('你是天后')
     socket.on('message', msg => {
         socket.send('server收到的： ' + msg)
     });
 });
 
-server.listen(80);
+io.of('/news').on('connection', socket => {
+    console.log('新闻服务端已经连接');
+    // 监听客户端发来的消息
+    socket.on('message', msg => {
+        console.log(msg);
+        // 向全世界的所有人广播
+        io.of('/news').emit('message', msg);
+    })
+});
+
+server.listen(3000);
+
+
+/* 
+    Socket.prototype.send = function() {
+        
+    }
+*/

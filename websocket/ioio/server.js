800e3bc4e2e324b3ed04d3e3a8aaabdf56e01b9d
chenhongdong <513948670@qq.com>
2018-10-22 08:39:29 +0800

    进入房间

--- websocket/ioio/server.js
+++ websocket/ioio/server.js
@@ -14,21 +14,47 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 const SYSTEM = '系统';
+
+let sockets = {};
 // 监听客户端与服务端的连接
 io.on('connection', function(socket) {
     // 用户名，默认为undefined
     let username;
     socket.on('message', function(msg) {
         if (username) {
-            // 服务器要向所有的客户端进行广播
-            io.emit('message', {
-                user: username,
-                content: msg,
-                createAt: new Date().toLocaleString()
-            })
+            // 首先要判断是私聊还是公聊
+            let result = msg.match(/@([^ ]+) (.+)/);
+            if (result) {
+                let toUser = result[1];
+                let content = result[2];
+                let toSocket = sockets[toUser];
+
+                if (toSocket) {
+                    toSocket.send({
+                        user: username,
+                        content,
+                        createAt: new Date().toLocaleString()
+                    });
+                } else {
+                    // 私聊的用户不在线的话，自己说一下
+                    socket.send({
+                        user: SYSTEM,
+                        content: '您私聊的用户不在线',
+                        createAt: new Date().toLocaleString()
+                    });
+                }
+            } else {
+                // 服务器要向所有的客户端进行广播
+                io.emit('message', {
+                    user: username,
+                    content: msg,
+                    createAt: new Date().toLocaleString()
+                });
+            }
         } else {
             // 如果第一次进来没有用户名，就把msg当做用户名
             username = msg;
+            sockets[username] = socket;
             // socket.broadcast表示向除自己以为的所有人广播
             socket.broadcast.emit('message', {
                 user: SYSTEM,
@@ -37,6 +63,11 @@ io.on('connection', function(socket) {
             });
         }
         
+    });
+    // 监听进入某个房间
+    socket.on('join', function(roomName) {
+        // socket.join表示进入某个房间
+        socket.join(roomName);
     });
 });
 

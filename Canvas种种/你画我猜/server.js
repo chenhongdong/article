e9055786eb061252e49ee9897ac7502b2433646b
unknown <chenhongdong@zwt-dc03.corp.qihoo.net>
2018-11-26 14:37:59 +0800

    'update'

--- Canvas种种/你画我猜/server.js
+++
@@ -1,6 +0,0 @@
-const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 9999 });
-
-ws.on('connection', socket => {
-    console.log(socket);
-});
--- Canvas种种/我画你猜/server.js
+++ Canvas种种/我画你猜/server.js
@@ -5,6 +5,8 @@ ws.on('connection', (socket, req) => {
     socket.send('哈哈');
     socket.on('message', data => {
         console.log('接收： ' + data);
+
+        socket.emit('message', data);
     });
     /* ws.clients.forEach(client => {
         console.log(client._receiver.Receiver);

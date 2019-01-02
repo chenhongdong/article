59ca29ffcb573467e379b8e5ff8d6c994abed080
unknown <chenhongdong@zwt-dc03.corp.qihoo.net>
2019-01-02 19:11:01 +0800

    again

--- Canvas种种/我画你猜/again/app.js
+++ Canvas种种/我画你猜/again/app.js
@@ -6,17 +6,134 @@ app.use(express.static(__dirname));
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// 常量用来区分是线段还是聊天
+const LINE = 0;
+const MESSAGE = 1;
+// ## 构建多人游戏
+const GAME_LOGIC = 2;
+const WAITING = 0;
+const GAME_START = 1;
+const GAME_OVER = 2;
+const GAME_RESTART = 3;
+
+let player = 0;
+
+const wordsList = ['苹果', '愤怒的小鸟', '火箭', '足球'];
+let currentAnswer;
+let currentState = WAITING;
+let gameOverTimeout;
+let playerNum = 0;
+
 // 用户列表
 const userList = ['皮卡丘', '伊布', '比比鸟', '巴大蝴', '小火龙', '妙蛙种子', '杰尼龟', '乘龙'];
-
 
 io.on('connection', socket => {
     // 随机创建用户
     const user = userList[Math.floor(Math.random() * userList.length)];
-
     let message = `欢迎${user}来到《我画你猜》的游戏世界！`;
+    // 获取所有连接的客户端数量
+    io.clients((err, client) => {
+        if (err) throw err;
+        playerNum = client.length;
+    });
 
-    io.emit('message', message);
+    // ### 通过socket发送绘图数据
+    let data = {};
+    data.type = MESSAGE;
+    data.sender = '系统';
+    data.message = message;
+    io.emit('message', JSON.stringify(data));
+    // ### 通过socket发送绘图数据
+    // ## 构建多人游戏
+    // 发送游戏状态给所有玩家
+    let gameData = {};
+    gameData.type = GAME_LOGIC;
+    gameData.gameState = WAITING;
+    io.emit('message', JSON.stringify(gameData));
+
+    // 如果有两个人以上的连接，就开始游戏
+    console.log('当前状态', currentState);
+    if (currentState === WAITING && playerNum >= 2) {
+        startGame(socket);
+    }
+    // ## 构建多人游戏
+
+    socket.on('message', msg => {
+        // 处理客户端发来的消息数据
+        let data = JSON.parse(msg);
+        if (data.type === MESSAGE) {
+            data.sender = user;
+        }
+        io.emit('message', JSON.stringify(data));
+
+        // ## 构建多人游戏
+        // 检测是否猜中答案
+        if (data.type === MESSAGE) {
+            // 走这里表示猜对了
+            if (currentState === GAME_START && data.message === currentAnswer) {
+                let gameData = {};
+                gameData.type = GAME_LOGIC;
+                gameData.gameState = GAME_OVER
+                gameData.winner = user;
+                gameData.answer = currentAnswer;
+
+                io.emit('message', JSON.stringify(gameData));
+
+                clearTimeout(gameOverTimeout);
+            }
+        }
+
+        if (data.type === GAME_LOGIC && data.gameState === GAME_RESTART) {
+            startGame(socket);
+        }
+        // ## 构建多人游戏
+    });
 });
+
+
+function startGame(socket) {
+    // 选择一个玩家让他来画画
+    player = (player + 1) % playerNum;
+    // 选择一个答案
+    let random = Math.floor(Math.random() * wordsList.length);
+    currentAnswer = wordsList[random];
+
+    // 所有玩家开始游戏
+    let data = {}
+    data.type = GAME_LOGIC;
+    data.gameState = GAME_START;
+    data.isPlayer = false;
+    io.emit('message', JSON.stringify(data));
+
+    // 游戏开始，玩家依次回答
+    let index = 0;
+    io.clients((err, client) => {
+        if (err) throw err;
+        client.forEach(item => {
+            if (index === player) {
+                let data2 = {};
+                data2.type = GAME_LOGIC;
+                data2.gameState = GAME_START;
+                data2.answer = currentAnswer;
+                data2.isPlayer = true;
+                socket.send(JSON.stringify(data2));
+            }
+            index++;
+        });
+    });
+
+    // 1分钟后游戏结束
+    gameOverTimeout = setTimeout(() => {
+        let newData = {};
+        newData.gameState = GAME_OVER;
+        newData.winner = '没有';
+        newData.answer = currentAnswer;
+        io.emit(JSON.stringify(newData));
+
+        currentState = WAITING;
+    }, 60 * 1000);
+
+    currentState = GAME_START;
+}
 
 server.listen(9000);
--- Canvas种种/我画你猜/again/index.js
+++ Canvas种种/我画你猜/again/index.js
@@ -1,5 +1,87 @@
-let gameObj = {};
+let gameObj = {
+    // 当前是否在进行绘图
+    isDrawing: false,
+    // 绘制下一条线的起点
+    startX: 0,
+    startY: 0,
+    // ### 通过socket发送绘图数据
+    LINE: 0,
+    MESSAGE: 1,
+    // ### 通过socket发送绘图数据
 
+    // ## 构建多人游戏
+    GAME_LOGIC: 2,
+    // 游戏逻辑状态
+    WAITING: 0,
+    GAME_START: 1,
+    GAME_OVER: 2,
+    GAME_RESTART: 3,
+    // 谁来进行绘图
+    isTurnToDraw: false
+    // ## 构建多人游戏
+};
+// 获取canvas上下文,这里不能用jq去获取,会找不到对应的canvas上下文
+const cvs = document.getElementById('canvas');
+const ctx = cvs.getContext('2d');
+
+
+// ### 绘制画板
+// 在canvas上进行绘图
+if (gameObj.isTurnToDraw) {
+    $('#canvas').on('mousedown', function(e) {
+        let pos = $(this).offset();
+
+        let mouseX = (e.pageX - pos.left) || 0;
+        let mouseY = (e.pageY - pos.top) || 0;
+
+        gameObj.startX = mouseX;
+        gameObj.startY = mouseY;
+        gameObj.isDrawing = true;
+    }).on('mousemove', function(e) {
+        // 当已经开始绘图时，才开始画线
+        if (gameObj.isDrawing) {
+            let pos = $(this).offset();
+
+            let mouseX = (e.pageX - pos.left) || 0;
+            let mouseY = (e.pageY - pos.top) || 0;
+
+            if (!(mouseX === gameObj.startX && mouseY === gameObj.startY)) {
+                drawLine(ctx, gameObj.startX, gameObj.startY, mouseX, mouseY, 1);
+
+                // #### 将画线数据打包成json
+
+                let data = {};
+                data.type = gameObj.LINE;
+                data.startX = gameObj.startX;
+                data.startY = gameObj.startY;
+                data.endX = mouseX;
+                data.endY = mouseY;
+                gameObj.socket.send(JSON.stringify(data));
+                // #### 将画线数据打包成json
+
+                gameObj.startX = mouseX;
+                gameObj.startY = mouseY;
+            }
+        }
+    }).on('mouseup', () => {
+        gameObj.isDrawing = false;
+    });
+}
+// 在起点和终点之间绘制线段
+function drawLine(ctx, x1, y1, x2, y2, thick) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineWidth = thick;
+    ctx.strokeStyle = '#00a1f4';
+    ctx.stroke();
+}
+
+// ### 绘制画板
+
+
+// ### 建立连接
+// 创建io去连接服务端
 gameObj.socket = io();
 
 gameObj.socket.on('connect', () => {
@@ -7,6 +89,74 @@ gameObj.socket.on('connect', () => {
 });
 
 gameObj.socket.on('message', msg => {
-    console.log(msg);
-    $('#chat-history').append(`<li>${msg}</li>`)
+    console.log(JSON.parse(msg));
+    // #### 接收画线数据来绘制到画板上
+    let data = JSON.parse(msg);
+    if (data.type === gameObj.MESSAGE) {
+        $('#chat-history').append(`<li><span class="user">${data.sender}</span>： ${data.message}</li>`)
+    } else if (data.type === gameObj.LINE) {
+        drawLine(ctx, data.startX, data.startY, data.endX, data.endY, 1);
+    } else if (data.type === gameObj.GAME_LOGIC) { // ## 构建多人游戏
+        // 如果游戏状态为结束，就展示获胜的选手
+        if (data.gameState === gameObj.GAME_OVER) {
+            gameObj.isTurnToDraw = false;
+            $('#chat-history').append(`<li><span class="winner">${data.winner}</span>获胜了，回答的是：<em>${data.answer}</em></li>`);
+            $('#restart').show();
+        }
+
+        if (data.gameState === gameObj.GAME_START) {
+            // 清空画布
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            // 隐藏重玩按钮
+            $('#restart').hide();
+            // 清空聊天记录
+            $('chat-history').html('');
+            
+            // 如果轮到了画图的选手，会看到不一样的文字提示
+            if (data.isPlayer) {
+                gameObj.isTurnToDraw = true;
+                $('#chat-history').append(`<li>请你用心的画出<span style="color: yellow">${data.answer}</span>吧</li>`);
+            } else {
+                $('#chat-history').append('<li>游戏即将开始，你有1分钟的时间去猜哦</li>');
+            }
+        }
+    }
+    // ## 构建多人游戏
+    // #### 接收画线数据来绘制到画板上
+    
 });
+// ### 建立连接
+
+// #### 发送消息
+
+// 点击发送消息
+$('#btn').click(sendMsg);
+// 回车发送消息
+$('#chat-input').keypress(e => {
+    if (e.keyCode === 13) {
+        sendMsg();
+    }
+});
+
+function sendMsg() {
+    let val = $('#chat-input').val();
+    let data = {};
+    data.type = gameObj.MESSAGE;
+    data.message = val;
+    if (val) {
+        gameObj.socket.send(JSON.stringify(data));
+        $('#chat-input').val('');
+    }
+}
+// #### 发送消息
+
+// #### 重玩
+$('#restart').click(restart);
+
+function restart() {
+    let data = {};
+    data.type = gameObj.GAME_LOGIC;
+    data.gameState = gameObj.GAME_RESTART;
+
+    gameObj.socket.send(JSON.stringify(data));
+}
